6dd9d5c22db6ed141bd1a65d5b8a25842fa61c6d
diclebahceli <diclebahceli24@gmail.com>
2025-02-01 20:46:11 +0300

    feat: make ids unique

--- BookStore/Core/BookStore.Domain/Entities/Book.cs
+++ BookStore/Core/BookStore.Domain/Entities/Book.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookStore.Domain.Entities;
 
 public class Book
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Title { get; set; }
     public Genre Genre { get; set; }
--- BookStore/WebApi/DBOperations/DataGenerator.cs
+++ BookStore/WebApi/DBOperations/DataGenerator.cs
@@ -18,7 +18,6 @@ public class DataGenerator
             context.Books.AddRange(
                 new Book()
                 {
-                    Id = 1,
                     Title = "The Great Gatsby",
                     Genre = new Genre() { Id = 1, Name = "Novel" },
                     PageCount = 180,
@@ -26,7 +25,6 @@ public class DataGenerator
                 },
              new Book()
              {
-                 Id = 2,
                  Title = "Moby",
                  Genre = new Genre() { Id = 2, Name = "Novel" },
                  PageCount = 200,
@@ -34,7 +32,6 @@ public class DataGenerator
              },
                 new Book()
                 {
-                    Id = 3,
                     Title = "Don Quixote",
                     Genre = new Genre() { Id = 3, Name = "Novel" },
                     PageCount = 863,
